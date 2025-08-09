9a6971a0f5159d2bdb244ff20d9359ab0e995072
Mhd-Osama <mdosamaali30@gmail.com>
2025-08-10 01:59:59 +0530

    Object Practice in js

---
+++ object.js
@@ -0,0 +1,15 @@
+var data={
+    Name:"Osama",
+    Age: [1,2,3],
+    Degree: "B.tech"
+        
+    }
+//Object function
+var Car={
+    Name: "Audi",
+    Model:"A6",
+    Price: "60 Lakhs",
+    fun: function(){
+        console.log("The name of the beast is" + this.Name)
+    }
+}
